c05c66663cb2e429ca3aa232e9e54e3e19dfdb66
sixroots <100560141+sixroots@users.noreply.github.com>
2024-04-04 15:33:04 +0200

    Fixed files - bug fix related to lack of update on Backstage

---
+++ node_modules/app/dist/static/5451.a6a8297a.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{(self.webpackChunkapp=self.webpackChunkapp||[]).push([[5451],{25451:(h,s,a)=>{a.r(s),a.d(s,{SearchPage:()=>E.S});var E=a(96377),P=a(14041)}}]);})();
---
+++ node_modules/app/dist/static/7700.862ae7a7.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{(self.webpackChunkapp=self.webpackChunkapp||[]).push([[7700],{47700:(E,a,s)=>{s.r(a),s.d(a,{SettingsPage:()=>t.a});var t=s(11851),P=s(14041)}}]);})();
